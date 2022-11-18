0e7802ec821a0bcd4828516ab7ee67ad47e0785b
HyungJin Han <han1210_36@naver.com>
2022-11-18 15:44:45 +0900

    파일 정리

--- frontend/src/components/FastAPI_test/ErrorMessage.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-function ErrorMessage(props) {
-  return (
-    <p className="has-text-weight-bold has-text-danger">{props.message}</p>
-  );
-}
-
-export default ErrorMessage;
